c8f166f03a82dea72985c61447742d8704a3c24d
jgallud <jgallud@gmail.com>
2018-09-19 10:04:42 +0200

    modelo v0.2

--- modelo.js
+++ modelo.js
@@ -8,15 +8,28 @@ function Juego(){
 	this.agregarUsuario=function(usuario){
 		this.usuarios.push(usuario);
 	}
+	this.obtenerColeccionInicial=function(numero){
+		return this.cartas.slice(0,numero);
+	}
+	this.iniciarColeccion=function(){
+		for(var i=0;i<3;i++){
+			this.agregarCarta(new Carta(5,new Azul(),"dragon"));
+			this.agregarCarta(new Carta(10,new Rojo(),"perro"));
+			this.agregarCarta(new Carta(15,new Amarillo(),"gato"));
+		}
+	}
+	this.iniciarColeccion();
 }
 
 function Usuario(nombre,juego){
 	this.nombre=nombre;
 	this.juego=juego;
 	this.mazo=[];
-	this.obtenerColeccionInicial=function(){
-		//this.mazo=this.juego.obtenerColeccionInicial();
+	this.numCartas=3;
+	this.obtenerColeccionInicial=function(numero){
+		this.mazo=this.juego.obtenerColeccionInicial(numero);
 	}
+	this.obtenerColeccionInicial(this.numCartas);
 }
 
 function Carta(vidas,color,nombre){
