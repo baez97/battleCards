cd8e1b931ea0a02a855534fbbdf00d51612c1e46
jgallud <jgallud@gmail.com>
2018-10-01 13:54:50 +0200

    test usr2

--- cliente/spec/modeloSpec.js
+++ cliente/spec/modeloSpec.js
@@ -1,10 +1,11 @@
 describe("El juego de las cartas...", function() {
   var juego;
-  var usr;
+  var usr1,usr2;
 
   beforeEach(function() {
     juego=new Juego();
-    usr=new Usuario("pepe");
+    usr1=new Usuario("pepe");
+    usr2=new Usuario("juan");
   });
 
   it("Compruebo condiciones iniciales (cartas, usuario)", function() {
@@ -14,21 +15,35 @@ describe("El juego de las cartas...", function() {
     expect(juego.usuarios.length).toEqual(0);
   });
 
-  it("El usuario tiene un mazo", function(){
-    expect(usr.mazo).toBeDefined();
-    expect(usr.mazo.length).toEqual(0);
+  it("Los usuarios tienen un mazo", function(){
+    expect(usr1.mazo).toBeDefined();
+    expect(usr1.mazo.length).toEqual(0);
+    expect(usr2.mazo).toBeDefined();
+    expect(usr2.mazo.length).toEqual(0);
     });
 
-    it("El usuario tiene mano (inicialmente sin cartas)", function(){
-    expect(usr.mano).toBeDefined();
-    expect(usr.mano.length).toEqual(0);
+    it("Los usuarios tiene mano (inicialmente sin cartas)", function(){
+    expect(usr1.mano).toBeDefined();
+    expect(usr1.mano.length).toEqual(0);
+    expect(usr2.mano).toBeDefined();
+    expect(usr2.mano.length).toEqual(0);
     });
 
-
-   it("agrego el usuario al juego", function(){
-    juego.agregarUsuario(usr);
+   it("agregar pepe el usuario al juego", function(){
+    juego.agregarUsuario(usr1);
     expect(juego.usuarios.length).toEqual(1);
     expect(juego.usuarios[0].nombre).toEqual("pepe");
-    expect(usr.mazo.length).toEqual(30);
+    expect(usr1.mazo.length).toEqual(30);
     });
+
+   it("agregar pepe y juan el usuario al juego", function(){
+    juego.agregarUsuario(usr1);
+    juego.agregarUsuario(usr2);
+    expect(juego.usuarios.length).toEqual(2);
+    expect(juego.usuarios[0].nombre).toEqual("pepe");
+    expect(usr1.mazo.length).toEqual(30);
+    expect(juego.usuarios[1].nombre).toEqual("juan");
+    expect(usr2.mazo.length).toEqual(30);
+    });
+
 });
--- servidor/modeloSpec.js
+++ servidor/modeloSpec.js
@@ -1,35 +1,49 @@
 var modelo = require('./modelo.js');
 describe("El juego de las cartas...", function() {
   var juego;
-  var usr;
+  var usr1,usr2;
 
   beforeEach(function() {
     juego=new modelo.Juego();
-    usr=new modelo.Usuario("pepe");
+    usr1=new modelo.Usuario("pepe");
+    usr2=new modelo.Usuario("juan");
   });
 
-  it("Compruebo condiciones iniciales (cartas, usuario)", function() {
+   it("Compruebo condiciones iniciales (cartas, usuario)", function() {
     expect(juego.cartas).toBeDefined();
     expect(juego.cartas.length).toEqual(30);
     expect(juego.usuarios).toBeDefined();
     expect(juego.usuarios.length).toEqual(0);
   });
 
-  it("El usuario tiene un mazo", function(){
-    expect(usr.mazo).toBeDefined();
-    expect(usr.mazo.length).toEqual(0);
+  it("Los usuarios tienen un mazo", function(){
+    expect(usr1.mazo).toBeDefined();
+    expect(usr1.mazo.length).toEqual(0);
+    expect(usr2.mazo).toBeDefined();
+    expect(usr2.mazo.length).toEqual(0);
     });
 
-    it("El usuario tiene mano (inicialmente sin cartas)", function(){
-    expect(usr.mano).toBeDefined();
-    expect(usr.mano.length).toEqual(0);
+    it("Los usuarios tiene mano (inicialmente sin cartas)", function(){
+    expect(usr1.mano).toBeDefined();
+    expect(usr1.mano.length).toEqual(0);
+    expect(usr2.mano).toBeDefined();
+    expect(usr2.mano.length).toEqual(0);
     });
 
-
-   it("agrego el usuario al juego", function(){
-    juego.agregarUsuario(usr);
+   it("agregar pepe el usuario al juego", function(){
+    juego.agregarUsuario(usr1);
     expect(juego.usuarios.length).toEqual(1);
     expect(juego.usuarios[0].nombre).toEqual("pepe");
-    expect(usr.mazo.length).toEqual(30);
+    expect(usr1.mazo.length).toEqual(30);
+    });
+
+   it("agregar pepe y juan el usuario al juego", function(){
+    juego.agregarUsuario(usr1);
+    juego.agregarUsuario(usr2);
+    expect(juego.usuarios.length).toEqual(2);
+    expect(juego.usuarios[0].nombre).toEqual("pepe");
+    expect(usr1.mazo.length).toEqual(30);
+    expect(juego.usuarios[1].nombre).toEqual("juan");
+    expect(usr2.mazo.length).toEqual(30);
     });
 });
