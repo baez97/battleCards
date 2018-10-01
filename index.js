29c4860194369fd76bdf74b5d03f8fcc848990e0
jgallud <jgallud@gmail.com>
2018-10-01 12:53:19 +0200

    jasmine-node

---
+++ index.js
@@ -0,0 +1,17 @@
+var fs=require("fs");
+var config=JSON.parse(fs.readFileSync("config.json"));
+var host=config.host;
+var port=config.port;
+var exp=require("express");
+var app=exp(); 
+var modelo=require("servidor/modelo.js")
+
+var juego=new Juego();
+
+app.get("/",function(request,response){
+	response.send("hola");
+});
+
+
+console.log("Servidor escuchando en "+host+":"+port);
+app.listen(port,host);
